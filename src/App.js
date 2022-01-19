b98a6ef3a06726a2bee812bf47424dcb3de83293
LavRed <74660422+LavRed@users.noreply.github.com>
2022-01-19 18:29:05 -0500

    Added footer to main code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        Weather App
+      <h1>Weather App</h1>
+      <footer>
+        <a
+          href="https://github.com/LavRed/vanilla-weather-app.git"
+          target="no-opener"
+        >
+        Open Source Code
+        </a>
+        by LaVisha Redmon
+      </footer>
+
     </div>
   );
 }
