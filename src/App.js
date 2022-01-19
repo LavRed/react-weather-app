e711e9fac00c904f38844df262a0a67ba3e5467e
LavRed <74660422+LavRed@users.noreply.github.com>
2022-01-19 18:49:53 -0500

    added bootstrap to the project

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        <a
-          href="https://github.com/LavRed/vanilla-weather-app.git"
-          target="no-opener"
-        >
-        Open Source Code
-        </a>
-        by LaVisha Redmon
-      </footer>
-
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          <a
+            href="https://github.com/LavRed/react-weather-app.git"
+            target="noopener"
+          >
+          Open Source Code {" "}
+          </a>
+          by LaVisha Redmon
+        </footer>
+      </div>
     </div>
   );
 }
