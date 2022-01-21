cff159b4da1e8846a4fb17d7c162657f8ffa758f
LavRed <74660422+LavRed@users.noreply.github.com>
2022-01-21 14:43:44 -0500

    added css styling and removed errors in manifest due to logos removed

---
+++ src/Weather.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+    return (
+        <div className="Weather">
+            <form>
+                <div className="row">
+                    <div className="col-9">
+                        <input type="search" 
+                        placeholder="Enter a city..." 
+                        className="form-control"
+                        autoFocus="on" 
+                        />
+                    </div>
+                    <div className="col-3">
+                        <input type="submit" 
+                        value="Search" 
+                        className="btn btn-primary w-100" />
+                    </div>
+                </div>
+            </form>
+            <h1>New York</h1>
+            <ul>
+                <li>Wednesday 04:00</li>
+                <li>Mostly Cloudy</li>
+            </ul>
+            <div className="row mt-3">
+                <div className="col-6">
+                    <div className="clearfix">
+                        <img
+                        src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                        alt="Partly Cloudy"
+                        />
+                            <span className="temperature">36</span>
+                            <span className="unit">°F</span>
+                    </div>
+                </div>
+                <div className="col-6">
+                    <ul>
+                        <li>Precipitation: 1%</li>
+                        <li>Humidity: 31%</li>
+                        <li>Wind: 13 km/h</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+}
