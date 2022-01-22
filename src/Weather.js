d775c9d0f7addc73e8a359cf755207a9a12dd493
LavRed <74660422+LavRed@users.noreply.github.com>
2022-01-22 18:23:06 -0500

    added the weather forecast and updated the format

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./Weather.css";
 
@@ -11,6 +12,7 @@ export default function Weather(props) {
         console.log(response.data);
         setWeatherData({
             ready: true,
+            coord: response.data.coord,
             temperature: response.data.main.temp,
             wind: response.data.wind.speed,
             date: new Date(response.data.dt * 1000),
@@ -58,6 +60,7 @@ export default function Weather(props) {
                 </div>
             </form>
             <WeatherInfo data={weatherData} />
+            <WeatherForecast coord={weatherData.coord} />
         </div>
     );
     } else {
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -8,17 +8,17 @@ export default function WeatherInfo(props) {
     return ( 
     <div className="WeatherInfo">        
         <h1>{props.data.city}</h1>
-            <div className="circle mt-3">
+            <div className="circle mt-3 mb-3">
                 <div className="circle-content">
             <ul>
-                <li>
-                    <FormatDate date={props.data.date} />
-                </li>
                 <li className="text-capitalize">{props.data.description}</li>
+                <li>                        
+                    <WeatherIcon code={props.data.icon} size={64} color={"#e7eaf6"} />
+                </li>
             </ul>
                     <div className="clearfix">
-                        <WeatherIcon code={props.data.icon} />
                         <WeatherTemperature fahrenheit={props.data.temperature} />
+                        <FormatDate date={props.data.date} />
                     </div>
                     <ul>
                         <li>Humidity: {props.data.humidity}%</li>
